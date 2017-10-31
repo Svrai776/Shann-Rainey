3442f2405e9573b3bc1d2ec64c4a9fa95e2898e6
Shann Rainey <shannvrainey@gmail.com>
2017-10-31 09:50:16 -0400

    Homework 6
    Added  the drop down list

--- Week_05_Advanced_Programming/Assignment/starter_code/js/index.js
+++ Week_05_Advanced_Programming/Assignment/starter_code/js/index.js
@@ -1,43 +1,52 @@
 $(document).ready(function() {
 
-	$("form").submit(function (event) {
+	var cityField = $("#city-type");
+	var cities = ['NYC', 'SF', 'LA', 'ATX', 'SYD'];
 
-		var inputField = $("input").val();
-		
-		if(inputField === "New York" ||
-		 inputField === "New York City"|| 
-		 inputField  ==="NYC") {
+	for (var i = 0; i < cities.length; i++) {
+		var city = cities[i];
+		var option = $('<option>' + city + '</option>');
+		option.val(city);
+		cityField.append(option);
+	}
+
+	$("form").submit(function (event) {
+		var cityFieldValue = cityField.val();
+
+		if(cityFieldValue === "New York" ||
+		 cityFieldValue === "New York City"|| 
+		 cityFieldValue  ==="NYC") {
 
 			$("body").css("background", "url('images/nyc.jpg')");
 			$("body").addClass("nyc");
 
 		} 
-		else if (inputField === "San Fransicso" ||
-		 inputField === "Bay Area"|| 
-		 inputField  ==="SF") {
+		else if (cityFieldValue === "San Fransicso" ||
+		 cityFieldValue === "Bay Area"|| 
+		 cityFieldValue  ==="SF") {
 
 			$("body").css("background", "url('images/sf.jpg')");
 			$("body").addClass("sf");
 
 		} 
-		else if (inputField === "Los Angeles" ||
-		 inputField === "LA"|| 
-		 inputField  ==="LAX") {
+		else if (cityFieldValue === "Los Angeles" ||
+		 cityFieldValue === "LA"|| 
+		 cityFieldValue  ==="LAX") {
 
 			$("body").css("background", "url('images/la.jpg')");
 			$("body").addClass("la");
 		} 
-		else if (inputField === "Austin" ||
-		 inputField === "ATX") {
+		else if (cityFieldValue === "Austin" ||
+		 cityFieldValue === "ATX") {
 
 			$("body").css("background", "url('images/austin.jpg')");
 			$("body").addClass("austin");
 
 		} 
-		else if (inputField === "Syndey"|| 
-		 inputField  ==="SYD") {
+		else if (cityFieldValue === "Sydney"|| 
+		 cityFieldValue  ==="SYD") {
 
-			$("body").css("background", "url('images/syndey.jpg')");
+			$("body").css("background", "url('images/sydney.jpg')");
 			$("body").addClass("syndey");
 		}
 
